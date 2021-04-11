afbd0342ff0b4e9025f66ddb130facb78b6699ea
markovesovic <mvesovic17@gmail.com>
2021-04-11 20:33:00 +0200

    Add response and error handling

---
+++ src/glue/src/common/errors/index.js
@@ -0,0 +1,73 @@
+class AuthenticationError extends Error {
+	constructor() {
+		super();
+		this.name = 'AuthenticationError';
+	}
+}
+
+class AuthorizationError extends Error {
+	constructor() {
+		super();
+		this.name = 'AuthorizationError';
+	}
+}
+
+class ValidationError extends Error {
+	constructor (mapped) {
+		super();
+
+		const val = {};
+		Object.keys(mapped).forEach(key => {
+			val[key] = {
+				location: mapped[key].location,
+				value: mapped[key].value,
+				message: mapped[key].msg
+			};
+		});
+
+		this.mapped = val;
+		this.name = 'ValidationError';
+	}
+
+	errors () {
+		return this.mapped;
+	}
+}
+
+class NotFoundError extends Error {
+	constructor() {
+		super();
+		this.name = 'NotFoundError';
+	}
+}
+
+class UserNotFoundError extends Error {
+	constructor() {
+		super();
+		this.name = 'UserNotFoundError';
+	}
+}
+
+class ConflictError extends Error {
+	constructor() {
+		super();
+		this.name = 'ConflictError';
+	}
+}
+
+class RegisterEmailConflict extends Error {
+	constructor() {
+		super();
+		this.name = 'RegisterEmailConflict';
+	}
+}
+
+module.exports = {
+	AuthenticationError,
+	AuthorizationError,
+	ValidationError,
+	NotFoundError,
+	UserNotFoundError,
+	ConflictError,
+	RegisterEmailConflict
+};
--- src/glue/src/index.js
+++ src/glue/src/index.js
@@ -8,3 +8,5 @@ app.set('PORT', process.env.PORT || 5000);
 app.listen(app.get('PORT'), () => {
 	logger.info(`Running on the ${app.get('PORT')} port.`);
 });
+
+module.exports = {};
